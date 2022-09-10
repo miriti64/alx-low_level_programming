57ec229e4f009db3d896c59d8b6c8f7b5cd5f0c1
miriti64 <mikiti64@gmail.com>
2022-09-10 04:17:45 -0700

    sdsds

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
  * main - print the string in the put function
- *
- * description: Using the main fuction this program prints "Programming is like bulding a multilingual puzzle"
+ * this program prints "Programming is like building a multilingual puzzle" 
  * Return: 0
  */
 int main(void)
