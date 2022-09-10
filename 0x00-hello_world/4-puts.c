62fa09a85ab90c600a40734bfe16672c3e4d973a
miriti64 <mikiti64@gmail.com>
2022-09-10 02:13:14 -0700

    sds

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #!/bin/bash
-<stdio.h>
+include <stdio.h>
 int main(void)
-{puts("\"programming is like building a multilingual puzzle");
-	return(0);
+{ 
+	puts ("\"programming is like building a multilingual puzzle");
+	return (0);
 }
 
