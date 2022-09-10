ab9810437357dc1acac203afcea1aba1ba5177bf
miriti64 <mikiti64@gmail.com>
2022-09-10 04:08:47 -0700

    sda

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 /**
- *
  * main - print the string in the put function
- *
- * Description: using the main function 
+ * Description: using the main function
  * this program prints "with proper grammar, but the outcome is a piece of art"
- * Return 0
+ * Return 0 (sucess)
  */
 int main(void)
 {
