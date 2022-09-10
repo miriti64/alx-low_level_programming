53bfc8753c65940d932341240fac32bd5e678134
miriti64 <mikiti64@gmail.com>
2022-09-10 05:09:59 -0700

    qw

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+/**
+ * main print the size of datatype
+ * Return: (o)
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+}
+printf("size of a char: %ld byte(s)\n", size of (c));
+printf("size of a int: %ld byte(s)\n", size of (i));
+printf("size of a long int: %ld byte(s)\n", size of (li));
+printf("size of a long long int: %ld byte(s)\n", size of (lli));
+printf("size of a float: %ld byte(s)\n", size of (f));
+return (0);
+}
