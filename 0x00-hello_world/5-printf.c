8e4d2970e870f225cd4d572883376f2690289dc2
miriti64 <mikiti64@gmail.com>
2022-09-10 04:02:08 -0700

    sdds

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
+ *
  * main - print the string in the put function
+ *
  * Description: using the main function 
  * this program prints "with proper grammar, but the outcome is a piece of art"
  * Return 0
