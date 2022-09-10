24a32ed76eb6a0ea17525e8fb2d8bb662d8f8500
miriti64 <mikiti64@gmail.com>
2022-09-10 05:19:34 -0700

    dfd

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
  * main print the size of datatype
  * Return: (o)
  */
+
 int main(void)
 {
 	char c;
@@ -11,10 +13,13 @@ int main(void)
 	long long lli;
 	float f;
 }
+
 printf("size of a char: %ld byte(s)\n", size of (c));
 printf("size of a int: %ld byte(s)\n", size of (i));
 printf("size of a long int: %ld byte(s)\n", size of (li));
 printf("size of a long long int: %ld byte(s)\n", size of (lli));
 printf("size of a float: %ld byte(s)\n", size of (f));
+
 return (0);
+
 }
