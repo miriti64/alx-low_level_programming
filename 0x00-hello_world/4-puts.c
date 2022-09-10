f5e8e9e58e8d68b81e5c1556b09c7732e8e77270
miriti64 <mikiti64@gmail.com>
2022-09-10 03:21:28 -0700

    juy

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #!/bin/bash
 # include <stdio.h>
+/**
+ * main - print the string in the put fucntion
+ *
+ * description : Using the main fuction this program prints "programming is like bulding a multilingual puzzle"
+ * Return : 0
+ */
 int main(void)
 { 
 	puts ("\"programming is like building a multilingual puzzle");
