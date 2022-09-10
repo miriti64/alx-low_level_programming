a88ca0fcc0fcd98421391b22532a320b36d30dbe
miriti64 <mikiti64@gmail.com>
2022-09-10 03:51:16 -0700

    olkk

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
-#!/bin/bash
 #include <stdio.h>
 /**
- * main - Entry point 
- *
- * Return : Always 0 (success)
+ * main - print the string in the put function
+ * Description: using the main function 
+ * this program prints "with proper grammar, but the outcome is a piece of art"
+ * Return 0
  */
 int main(void)
 {
