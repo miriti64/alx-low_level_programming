cefa6c1c2273de7f03519af056b204d1fea48664
miriti64 <mikiti64@gmail.com>
2022-09-10 04:19:49 -0700

    pace

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - print the string in the put function
- * this program prints "Programming is like building a multilingual puzzle" 
+ * this program prints "Programming is like building a multilingual puzzle"
  * Return: 0
  */
 int main(void)
