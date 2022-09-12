07a8b0637eb8fd22636bc20e883fc59fc7ed0692
miriti64 <mikiti64@gmail.com>
2022-09-12 09:23:07 -0700

    dsd

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: using the main function
+ * this program prints "Programming is positive, zero, or negative
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+
+	{
+		putchar(ch);
+	}
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
