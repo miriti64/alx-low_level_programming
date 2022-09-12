8d81e55f0213c706d3b8f6d43ca384a728241938
miriti64 <mikiti64@gmail.com>
2022-09-12 09:37:51 -0700

    23

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point  
+ * Return: 0
+ *
+ */
+
+int main(void)
+
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
