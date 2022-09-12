f3f63505ac77de154944a00a54a10672f3cb7d55
miriti64 <mikiti64@gmail.com>
2022-09-12 09:11:40 -0700

    lkj

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ 
+ * main - Entry point
+ * Description: using the main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+			putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
