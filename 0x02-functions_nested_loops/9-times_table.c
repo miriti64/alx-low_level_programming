1a4d5d98acfbe4e840febdff1501a932130fcb1e
miriti64 <mikiti64@gmail.com>
2022-09-14 08:00:15 -0700

    git

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,6 +8,7 @@ void times_table(void)
 	int num, mult, prod;
 
 	for (num = 0; num <= 9; num++)
+
 		{
 			_putchar('0');
 		
@@ -15,7 +16,7 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-	
+
 				prod = num * mult;
 				if (prod <= 9)
 				_putchar(' ');
