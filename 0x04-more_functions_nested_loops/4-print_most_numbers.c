7e50b95ec31fbe42015c134b102f775d2e02fe0b
miriti64 <mikiti64@gmail.com>
2022-09-16 07:06:34 -0700

    kijuhy

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,12 +10,11 @@ void print_most_numbers(void)
 {
 	int n;
 	for (n = 48; n < 58; n++)
-
 	{
 	if ((n == 50) || (n == 52))
-	{
-	continue;
-	}
+		{
+			continue;
+		}
 		putchar(n);
 	}
 	putchar(10);
