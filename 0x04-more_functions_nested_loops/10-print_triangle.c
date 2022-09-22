073e752ad422fdde8b39dc3f3bb82bbf32e8f8db
miriti64 <mikiti64@gmail.com>
2022-09-22 02:16:57 -0700

    task

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,28 +1,40 @@
 #include <stdio.h>
+#include "main.h"
 
-int main(void)
+/**
+ *print_triangle - prints a triangle
+ *@size:size parameter of triangle
+ *Return: returns nothing
+ */
+
+void print_triangle(int size)
 
 {
-	int n;
-	int a[5];
-	int *p;
+	int inc1, inc2;
 
-	a[2] = 1024;
-	p = &n;
-/*
-*
-* write your line of code here...
-* Remember:
-* - you are not allowed to use a
-* - you are not allowed to modify p
-* - only one statement
-* - you are not allowed to code anything else than this line of code
-*/
-*(p + 5) = 98;
-/* ...so that this prints 98\n */
+	if (size > 0)
+	{
+		for (inc1 = 1; inc1 <= size; inc1++)
+			{
+				for ((inc2 = size - inc1); inc2 > 0; inc2--)
+					{
+						putchar(' ');
+					}
 
-printf("a[2] = %d\n", a[2]);
+				for (inc2 = 0; inc2 < inc1; inc2++)
 
-return (0);
+					{
+						putchar('#');
+					}
+				if (inc1 == size)
+				{
+				continue;
+				}
+				putchar('\n');
+
+			}
+
+	}
+	putchar('\n');
 
 }
