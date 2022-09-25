e4322d428646c9ffc626ba1220cc4aa2ac34c1dd
miriti64 <mikiti64@gmail.com>
2022-09-25 02:02:01 -0700

    kill

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,30 +1,28 @@
 #include <stdio.h>
 #include "main.h"
+/**                                                                                                                                   
+ * rev_string - Reverses a string.                                                                                                    
+ * @s: The string to be reversed.                                                                                                     
+ */                                                                                                                                   
 
-/**
- * rev_string - Reverses a string
- * @s: string to be reversed
- * Return: nothing
- */
-void rev_string(char *s)
+ void rev_string(char *s)                                                                                                              
 
-{
-	int i, j, k, temp;
+{                                                                                                                                     
+        int len = 0, index = 0;                                                                                                       
+        char tmp;                                                                                                                     
+                                                                                                                                      
+        while (s[index++])                                                                                                            
+                len++;                                                                                                                
+                                                                                                                                 
+        for (index = len - 1; index >= len / 2; index--)                                                                              
+        {                                                                                                                             
+               tmp = s[index];                                                                                                       
+               s[index] = s[len - index - 1];                                                                                        
+               s[len - index - 1] = tmp;                                                                                             
+	}                                                                                                                             
 
-	i = 0;
-	while (s[i] != '\0')
-	{
-		i++
-	}
-	k = 0;
-	j = i - 1;
-	while (k < j)
-	{
-		temp = s[k];
-		s[k] = s[j];
-		s[j] = temp;
-		k++;
-		j++;
-	}
 }
+
+
+
 
